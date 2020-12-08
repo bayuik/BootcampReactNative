e840bcdc5beebd181e24b4cc189f9acd5224a0fc
bayuik <bayuindrakusuma05@gmail.com>
2020-12-09 04:41:25 +0700

    update rules skeleton code

--- Tugas-2/conditional.js
+++ Tugas-2/conditional.js
@@ -24,48 +24,52 @@ console.log('\n');
 
 //Switch Case
 
-var hari = 21;
+var hari = 22;
 var bulan = 12;
 var tahun = 2020;
 
-switch(bulan) {
-    case 1:
-        console.log(`${hari} Januari ${tahun}`);
-        break;
-    case 2:
-        console.log(`${hari} Februari ${tahun}`);
-        break;
-    case 3:
-        console.log(`${hari} Maret ${tahun}`);
-        break;
-    case 4:
-        console.log(`${hari} April ${tahun}`);
-        break;
-    case 5:
-        console.log(`${hari} Mei ${tahun}`);
-        break;
-    case 6:
-        console.log(`${hari} Juni ${tahun}`);
-        break;
-    case 7:
-        console.log(`${hari} Juli ${tahun}`);
-        break;
-    case 8:
-        console.log(`${hari} Agustus ${tahun}`);
-        break;
-    case 9:
-        console.log(`${hari} September ${tahun}`);
-        break;
-    case 10:
-        console.log(`${hari} Oktober ${tahun}`);
-        break;
-    case 11:
-        console.log(`${hari} November ${tahun}`);
-        break;
-    case 12:
-        console.log(`${hari} Desember ${tahun}`);
-        break;
-    default:
-        console.log(`Input bulan salah`);
-        break;
+if(hari >= 1 && hari <= 31 && bulan >= 1 && bulan <= 12 && tahun >= 1900 && tahun <= 2200){
+    switch(bulan) {
+        case 1:
+            console.log(`${hari} Januari ${tahun}`);
+            break;
+        case 2:
+            console.log(`${hari} Februari ${tahun}`);
+            break;
+        case 3:
+            console.log(`${hari} Maret ${tahun}`);
+            break;
+        case 4:
+            console.log(`${hari} April ${tahun}`);
+            break;
+        case 5:
+            console.log(`${hari} Mei ${tahun}`);
+            break;
+        case 6:
+            console.log(`${hari} Juni ${tahun}`);
+            break;
+        case 7:
+            console.log(`${hari} Juli ${tahun}`);
+            break;
+        case 8:
+            console.log(`${hari} Agustus ${tahun}`);
+            break;
+        case 9:
+            console.log(`${hari} September ${tahun}`);
+            break;
+        case 10:
+            console.log(`${hari} Oktober ${tahun}`);
+            break;
+        case 11:
+            console.log(`${hari} November ${tahun}`);
+            break;
+        case 12:
+            console.log(`${hari} Desember ${tahun}`);
+            break;
+        default:
+            console.log(`Input bulan salah`);
+            break;
+    }
+} else {
+    console.log(`Input tidak valid`)
 }
