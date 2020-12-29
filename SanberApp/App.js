21383faddc1c2b7a1a015eb244380455aa9a0974
bayuik <bayuindrakusuma05@gmail.com>
2020-12-30 04:45:56 +0700

    Add Tugas-15, React Native Navigation

--- SanberApp/App.js
+++ SanberApp/App.js
@@ -5,6 +5,7 @@ import LoginScreen from './Tugas/Tugas-13/LoginScreen';
 import AboutScreen from './Tugas/Tugas-13/AboutScreen';
 import RegisterScreen from './Tugas/Tugas-13/Register';
 import App14 from './Tugas/Tugas-14/App14';
+import App15 from './Tugas/Tugas-15/App15';
 
 export default function App() {
   return (
@@ -12,10 +13,7 @@ export default function App() {
     // <RegisterScreen />
     // <LoginScreen />
     // <AboutScreen />
-    <App14 />
+    // <App14 />
+    <App15 />
   );
-}
-
-const test = () => {
-  console.log('a')
 }
